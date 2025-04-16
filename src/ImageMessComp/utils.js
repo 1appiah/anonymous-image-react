2613b685c55740344b3b8f30122576092e65786b
1appiah <92669349+1appiah@users.noreply.github.com>
2025-04-16 01:11:27 +0000

    dajs js error solved

--- src/ImageMessComp/utils.js
+++ src/ImageMessComp/utils.js
@@ -1,3 +1,6 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 export const timeAgo = (timestamp) => {
     const now = dayjs();
     const time = dayjs(timestamp);
